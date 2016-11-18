810ce64156876d3b217c45cc7add34898de4df8a
Surya Oktafendri <f2face@outlook.com>
2016-11-18 11:25:17 +0700

    Add 'save' feature & improve code

--- src/f2face/WaybackMachine.php
+++ src/f2face/WaybackMachine.php
@@ -39,12 +39,44 @@ class WaybackMachine
         
         return json_decode($data);
     }
-
-    protected function getUrl($url, $options = array()) {
+    
+    /**
+     * Save a web page to Archive.org Wayback Machine.
+     * 
+     * @param string $url
+     * 
+     * @return string
+     */    
+    public function save($url) {
+        if (!preg_match('#^https?://#', $url))
+            throw new \Exception('Invalid URL.');
+        
+        $url = 'https://web.archive.org/save/' . $url;
+        
+        $data = $this->getUrl($url, array(), true);
+        
+        if (!array_key_exists('content-location', $data))
+            throw new \Exception('An error occured. Page saving failed.');
+        
+        return 'https://web.archive.org' . $data['content-location'];
+    }
+    
+    /**
+     * Get web page via cURL.
+     * 
+     * @param string $url
+     * @param array $options
+     * @param boolean $headers_only
+     * 
+     * @return string|array
+     */
+    protected function getUrl($url, $options = array(), $headers_only = false) {
         $opt = array(
-            'timeout' => 60,
-            'useragent' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
-            'parse' => false,
+            'CURLOPT_SSL_VERIFYPEER' => false,
+            'CURLOPT_SSL_VERIFYHOST' => false,
+            'CURLOPT_CONNECTTIMEOUT' => 60,
+            'CURLOPT_TIMEOUT' => 60,
+            'CURLOPT_USERAGENT' => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
         );
         
         if (!empty($options) && is_array($options))
@@ -54,19 +86,66 @@ class WaybackMachine
             throw new \Exception('Invalid URL.');
         
         $a = curl_init();
+        curl_setopt_array($a, $this->mapCurlOptArray($opt));
         curl_setopt($a, CURLOPT_URL, $url);
-        curl_setopt($a, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($a, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($a, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($a, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($a, CURLOPT_RETURNTRANSFER, true);
+        if ($headers_only) {
+            curl_setopt($a, CURLOPT_HEADER, true);
+            curl_setopt($a, CURLOPT_VERBOSE, true);
+        }
         
         $out = curl_exec($a);
         
+        if (!$out)
+            throw new \Exception(curl_error($a));
+        
+        if ($headers_only)
+            return $this->getCurlResponseHeaders($out);
+        
         curl_close($a);
         
-        if (!$out)
-            throw new \Exception('Error retrieving URL.');
+        return $out;
+    }
+    
+    /**
+     * Parse getURL() options to cURL options.
+     * 
+     * @param array $array
+     * 
+     * @return array
+     */
+    private function mapCurlOptArray($array) {
+        $out = array();
+        
+        foreach ($array as $key => $value) {
+            $out[constant($key)] = $value;
+        }
         
         return $out;
+    }
+    
+    /**
+     * Parse cURL response headers.
+     * 
+     * @param string $response
+     * 
+     * @return array
+     */
+    private function getCurlResponseHeaders($response) {
+        $headers = array();
+        
+        $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+        
+        foreach (explode("\r\n", $header_text) as $i => $line) {
+            if ($i === 0)
+                $headers['http_code'] = $line;
+            else {
+                list ($key, $value) = explode(': ', $line);
+                $headers[strtolower($key)] = $value;
+            }
+        }
+        
+        return $headers;
     }
 }
